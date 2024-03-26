8b78410438455b6fbdd40c76e7c3f0e4cc63a3a9
isLinXu <2267379130@qq.com>
2024-03-27 03:15:37 +0800

    [TNN.Demo] init demo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include "tnn/utils/blob_converter.h"
 #include "tnn/utils/mat_utils.h"
 #include "tnn/utils/dims_vector_utils.h"
+#include "opencv4/opencv2/opencv.hpp"
+#include "opencv4/opencv2/core.hpp"
+using namespace std;
+using namespace cv;
 using namespace TNN_NS;
 
 int main() {
